b275e7c5f800f30976e3d85840d9c498d6da50be
yayEverest <wheels8newchannel@gmail.com>
2024-11-29 20:37:41 -0500

    the item fr

--- takeflight/src/main/java/evy/evy/takeflight/Takeflight.java
+++ takeflight/src/main/java/evy/evy/takeflight/Takeflight.java
@@ -2,6 +2,7 @@ package evy.evy.takeflight;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+
 public final class Takeflight extends JavaPlugin {
 
     @Override
---
+++ takeflight/src/main/java/evy/evy/takeflight/items/FlightStick.java
@@ -0,0 +1,11 @@
+package evy.evy.takeflight.items;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+public class FlightStick {
+    public static ItemStack getFlightStick() {
+        ItemStack flightStick = new ItemStack(Material.STICK);
+        return flightStick;
+    }
+}
